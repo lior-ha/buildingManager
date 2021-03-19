af05e98d215d06ada09b9f070321184e25e4739a
Lior <liorha@gmail.com>
2021-03-19 09:31:24 +0200

    added feature for the list of aprts in homepage to show total payment status(red/green)

--- src/components/apartments-payment-status/apartments-payment-status.component.jsx
+++ src/components/apartments-payment-status/apartments-payment-status.component.jsx
@@ -4,22 +4,52 @@ import Loading from '../UI/loader/loader.component';
 
 import './apartments-payment-status.styles.scss';
 
-const AptsPaymentStatus = ({loading, apartments}) => {
+const curYear = new Date().getFullYear();
+const curMonth = new Date().getMonth();
 
+const AptsPaymentStatus = ({loading, apartments}) => {
+    const apartmentsList = apartments
+        .sort((a,b) => a.apartment - b.apartment)
+        .map(apartment => {
+            let statusClass = 'green';
+            let statusPos = '';
+
+            // If current year was't added yet
+            if (!apartment.paymentsStatus[curYear]) {
+                apartment.paymentsStatus[curYear] = [...Array(12).fill('')]
+            }
+            
+            for (let key in apartment.paymentsStatus) {
+                if (curYear !== parseInt(key)){
+                    statusPos = apartment.paymentsStatus[key].find(debt => debt !== 'paid');
+                    console.log(apartment.apartment, 'prev year', key, statusPos);
+                } else if (curYear === parseInt(key)) {
+                    statusPos = apartment.paymentsStatus[key].find((debt, i) => {
+                        let result;
+                        if (i <= curMonth) {
+                            result = debt !== 'paid';
+                        }
+                        return result
+                    })
+                    console.log(apartment.apartment, 'cur year', key, statusPos)
+                }
+                if (statusPos !== undefined) {
+                    //console.log('statusPos', statusPos)
+                    statusClass = 'red';
+                }
+            }
+            return (
+                <Link key={apartment.id} id={apartment.aparment} to={`building/${apartment.id}`} className={`custom-button ${statusClass}`}>דירה {apartment.apartment} - {apartment.apartmentName}</Link>
+            )
+        }
+    )
     return (
         <div className="aptsPaymentStatus">
             { loading ? 
                 <Loading /> 
             : 
                 <div className="contentBox linkListBox">
-                    {apartments
-                        .sort((a,b) => a.apartment - b.apartment)
-                        .map(apartment => {
-                        //apartment.paymentStatus.
-                        return (
-                            <Link key={apartment.id} to={`building/${apartment.id}`} className={`custom-button`}>דירה {apartment.apartment} - {apartment.apartmentName}</Link>
-                        )
-                    })}
+                    {apartmentsList}
                 </div>
             }
         </div>
